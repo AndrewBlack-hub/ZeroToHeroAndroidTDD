20d6d694d6b2ac6d6fc25ac5b06f6a43f3507670
Andrew Sveshnikov <crys2014@mail.ru>
2023-11-18 18:41:31 +0700

    task/010 test passed with state pattern

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,50 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
+    private var state: State = State.Initial
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayout: LinearLayout = findViewById(R.id.rootLayout)
+        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        val removeButton: Button = findViewById(R.id.removeButton)
+
+        removeButton.setOnClickListener {
+            state = State.Removed
+            state.apply(linearLayout = linearLayout, textView = titleTextView, button = removeButton)
+        }
+
+        savedInstanceState?.let {
+            state = it.getSerializable("key") as State
+            state.apply(linearLayout, titleTextView, removeButton)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("key", state)
+    }
+}
+
+interface State : Serializable {
+    fun apply(linearLayout: LinearLayout, textView: TextView, button: Button)
+
+    object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) = Unit
+    }
+
+    object Removed : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) {
+            linearLayout.removeView(textView)
+            button.isEnabled = false
+        }
     }
 }
