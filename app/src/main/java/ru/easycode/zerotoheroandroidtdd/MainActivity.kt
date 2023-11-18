1c23bd8bcc66247215d12746c38771891d402de3
Andrew Sveshnikov <crys2014@mail.ru>
2023-11-18 16:27:02 +0700

    task/007 test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        val button: Button = findViewById(R.id.changeButton)
+        button.setOnClickListener {
+            textView?.text = "I am an Android Developer!"
+        }
+        savedInstanceState?.let {
+            textView?.text = it.getString("key")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", textView?.text.toString())
     }
 }
