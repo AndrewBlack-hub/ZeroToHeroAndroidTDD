0a28073974f5522f6be3ecc6a0376adc4d4377c9
Andrew Sveshnikov <crys2014@mail.ru>
2023-11-18 15:07:12 +0700

    task/005 test passed(programmatically)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,35 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        initUi()
+    }
+
+    private fun initUi() {
+        val layout = LinearLayout(this).apply {
+            id = R.id.rootLayout
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
+        }
+        val textView = TextView(this).apply {
+            gravity = Gravity.CENTER
+            id = R.id.titleTextView
+            text = "I am an Android Developer!"
+        }
+        val buttonView = Button(this).apply {
+            id = R.id.changeButton
+            text = "change"
+        }
+        layout.addView(textView)
+        layout.addView(buttonView)
+        setContentView(layout)
     }
 }
